c641858aeb32416f24377f1db352967308069017
dhairya-2004 <135092481+dhairya-2004@users.noreply.github.com>
2024-01-09 21:02:46 +0530

    Main page

---
+++ Files/React/src/Components/Home/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+
+const YourComponent = () => {
+     return (
+       <>
+        <header>
+          {/* <img  src={} alt=""></img> */}
+          <h2 id='catalyst'>Interview Catalyst</h2>
+          <nav >
+            <div className='navigation'>
+              <a href='/demo/'>Demo</a>
+            </div>
+            <div className='navigation'>
+              <a href='/demo/'>Demo</a>
+            </div>
+            <div className='navigation'>
+              <a href='/Write/'>Write</a>
+            </div>
+            <div className='navigation'>
+              <a href='/GetStarted/'>Get Started</a>
+            </div>
+
+
+          </nav>
+        </header>
+        <div className='container'>
+          <div className='wrapper'><h1>Ask , Chat,<br/>Answer , Repeat.</h1></div>
+          <div className='wrapper1'><h2> Ask questions and get real answers from real people.</h2></div>
+          <button className='btn'>Start Reading</button>
+        </div>
+
+
+       </>
+     );
+}; 
+
+export default YourComponent;
