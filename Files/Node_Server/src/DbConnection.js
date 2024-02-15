d9da6cfec33c3e261c3e6a8d2f42fdf79259c016
jash2504 <asmanijash51@gmail.com>
2024-02-16 00:26:33 +0530

    Done

--- Files/Node_Server/src/DbConnection.js
+++ Files/Node_Server/src/DbConnection.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/Interview-catalyst');
-        console.log("Connected");
+        await mongoose.connect('mongodb+srv://jashasmani:jashasmani@cluster0.mf0pimy.mongodb.net/Interview-Catalyst', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to MongoDB Atlas");
 
-    }
-    catch (e) {
-        console.log(e);
+    } catch (e) {
+        console.error("Error connecting to MongoDB Atlas", e);
     }
 }
 
-module.exports=connectDB;
-
+module.exports = connectDB;
