31a016595bce9b5c7549530e41a5175687596ada
jash2504 <asmanijash51@gmail.com>
2024-03-16 09:18:19 +0530

    Done

--- React/src/Components/Navbar/NavBar.jsx
+++ React/src/Components/Navbar/NavBar.jsx
@@ -18,10 +18,6 @@ const Navbar = () => {
         <img src="" alt="" className="logo" />
         <span className="site-name">Interview Catalyst</span>
       </div>
-      <div className="search-write">
-        {/* <img src={search} className="image-write" /> */}
-        <input type="search" placeholder="Search.." id="search" required />
-      </div>
       <div className={`buttons-container ${isMobileMenuOpen ? "show" : ""}`}>
         <NavLink to="/signin" className="button" activeClassName="active">
           Signup
@@ -30,12 +26,7 @@ const Navbar = () => {
         <NavLink to="/signin" className="button" activeClassName="active">
           Login
         </NavLink>
-        <input type="checkbox" id="toggle" />
-        <label id="switch" for="toggle">
-          <div id="circle"></div>
-          {/* <div id="text">Light mode</div>
-          <div id="text2">Dark mode</div> */}
-        </label>
+        
       </div>
       <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
         &#9776;
